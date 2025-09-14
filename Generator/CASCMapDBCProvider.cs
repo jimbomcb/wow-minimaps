7629d83a23e758703ecf9fc61f2bef547496dc47
John McBroom <john@mcbroom.dev>
2025-09-14 14:25:03 -0700

    Note about dbcprovider

--- Generator/CASCMapDBCProvider.cs
+++ Generator/CASCMapDBCProvider.cs
@@ -3,7 +3,8 @@ using DBCD.Providers;
 
 namespace Minimaps.Generator;
 
-// From https://github.com/Marlamin/WoWTools.Minimaps/blob/master/WoWTools.MinimapExtract/CASCDBCProvider.cs, todo: what are the magic numbers? what is 1349477? is it always the map db?
+// From https://github.com/Marlamin/WoWTools.Minimaps/blob/master/WoWTools.MinimapExtract/CASCDBCProvider.cs,
+// todo: what are the magic numbers? what is 1349477? - I think it's the file ID of the map db2, seems stable enough to hard-code?
 internal class CASCMapDBCProvider(CASCHandler casc) : IDBCProvider
 {
 	public Stream StreamForTableName(string tableName, string build)
