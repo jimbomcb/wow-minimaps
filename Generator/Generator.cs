7197c030cdfb5efc157080d0303ba48f33c973f5
John McBroom <john@mcbroom.dev>
2025-09-14 15:21:45 -0700

    Strip CascLib, use TACTSharp

--- Generator/Generator.cs
+++ Generator/Generator.cs
@@ -1,5 +1,4 @@
 ﻿using BLPSharp;
-using CASCLib;
 using DBCD;
 using DBCD.Providers;
 using Microsoft.Extensions.Logging;
@@ -18,7 +17,6 @@ internal class Generator
 	private readonly GeneratorConfig _config;
 	private readonly ILogger _logger;
 	private readonly CancellationToken _cancellationToken;
-	private CASCHandler _cascHandler = null;
 	private BuildInstance _buildInstance = null;
 
 	public readonly record struct MinimapData(int mapId, List<MinimapTile> tiles);
@@ -39,7 +37,6 @@ internal class Generator
 
 	internal async Task Generate()
 	{
-		//  TEMP TEMP: Swapping CASCLib with TACTSharp
 		_buildInstance = new BuildInstance();
 		_buildInstance.Settings.CacheDir = _config.CachePath;
 		_buildInstance.Settings.BaseDir = "C:\\World of Warcraft";
@@ -49,14 +46,7 @@ internal class Generator
 
 		_logger.LogInformation("Generating minimap data... product={product}, region={cascRegion}", _config.Product, _config.CascRegion);
 
-		var cascHandlerTask = GenerateHandler();
-		var tactKeysTask = LoadTACT();
-
-		CASCLib.KeyService.LoadKeys(await tactKeysTask);
-		_cascHandler = await cascHandlerTask;
-
-		// Set up DBCD
-		var dbcd = new DBCD.DBCD(new CASCMapDBCProvider(_cascHandler), new GithubDBDProvider());
+		var dbcd = new DBCD.DBCD(new TACTMapDBCProvider(_buildInstance), new GithubDBDProvider());
 		var mapDB = dbcd.Load("Map");
 		if (mapDB.Count == 0)
 			throw new Exception("No maps found in Map DBC");
@@ -102,65 +92,6 @@ internal class Generator
 		_logger.LogInformation("Generation complete.");
 	}
 
-	private async Task<CASCHandler> GenerateHandler()
-	{
-		CASCConfig.LoadFlags = LoadFlags.FileIndex;
-		CASCConfig.ThrowOnFileNotFound = true;
-		CASCLib.Logger.Init(); // Ideally feed to ILogger rather than the odd bespoke _logger
-		CDNCache.CachePath = Path.Join(_config.CachePath, "CASC");
-
-		var handler = _config.UseOnline ? CASCHandler.OpenOnlineStorage(_config.Product, _config.CascRegion) : CASCHandler.OpenLocalStorage("C:\\World of Warcraft", _config.Product);
-		handler.Root.SetFlags(LocaleFlags.enUS);
-
-		_logger.LogInformation("Initialized CASCHandler for build {build}", handler.Config.BuildName);
-
-		// Blocking main, todo: investigate async open
-
-		return handler;
-	}
-
-	private async Task<string> LoadTACT()
-	{
-		using var httpClient = new HttpClient();
-
-		string? cachedETag = null;
-		if (File.Exists(Path.Combine(_config.CachePath, "TACTKeys.txt.etag")))
-		{
-			cachedETag = await File.ReadAllTextAsync(Path.Combine(_config.CachePath, "TACTKeys.txt.etag"));
-			_logger.LogTrace("Found cached TACTKeys ETag: {ETag}", cachedETag);
-			httpClient.DefaultRequestHeaders.TryAddWithoutValidation("If-None-Match", cachedETag);
-		}
-
-		var tactRequest = await httpClient.GetAsync("https://github.com/wowdev/TACTKeys/raw/master/WoW.txt");
-		if (tactRequest.StatusCode == System.Net.HttpStatusCode.NotModified)
-		{
-			_logger?.LogInformation("TACTKeys not modified since last fetch, using cached version.");
-			return Path.Combine(_config.CachePath, "TACTKeys.txt");
-		}
-
-		tactRequest.EnsureSuccessStatusCode(); // If it's not NotModified, it should be a valid 200
-		var newETag = tactRequest.Headers.ETag?.Tag ?? throw new Exception("No ETag header in TACTKeys response");
-		_logger.LogInformation("Fetched new TACTKeys (etag {tag})", newETag);
-
-		// The TACTKeys repo provides [Name][Space][Value] and CascLib is hard-coded to take ; separated, so bridge the gap...
-		// "The format is a space separated file with the 16-char key lookup (or name) and the 32-char key itself, both encoded as hex."
-		// "More fields might be added at the end of the line in the future(e.g.IDs), be sure to only read the necessary data per line."
-		var tactContent = await tactRequest.Content.ReadAsStringAsync();
-		var cleanTact = "";
-		foreach (var line in tactContent.Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
-		{
-			if (line.IndexOf(' ') != 16) throw new Exception("Unexpected TACTKeys line format: " + line);
-			cleanTact += line[..16] + ';' + line[17..49] + '\n';
-		}
-
-		await Task.WhenAll(
-			File.WriteAllTextAsync(Path.Combine(_config.CachePath, "TACTKeys.txt"), cleanTact),
-			File.WriteAllTextAsync(Path.Combine(_config.CachePath, "TACTKeys.txt.etag"), newETag)
-		);
-
-		return Path.Combine(_config.CachePath, "TACTKeys.txt");
-	}
-
 	private async Task<MinimapData> ProcessMapRow(int mapId, DBCDRow dbRow)
 	{
 		var mapData = new MinimapData(mapId, new());
@@ -179,12 +110,16 @@ internal class Generator
 		_logger.LogInformation("Map {id}: {name} WDT:{wdt}", mapId, name, wdtFileId);
 
 		// TODO: How do we store and represent to the browser that that a maps WDT data is referenced
+		// TODO: Can we not more gracefully stream the WDT from TACTSharp rather than just loading the whole byte array..?
+
+
 		Stream fileStream;
 		try
 		{
-			fileStream = _cascHandler.OpenFile(wdtFileId);
+			var fileBytes = _buildInstance.OpenFileByFDID((uint)wdtFileId);
+			fileStream = new MemoryStream(fileBytes);
 		}
-		catch (FileNotFoundException ex)
+		catch (Exception ex)
 		{
 			throw new MapGenerationException(mapId, $"Map {mapId} ({name}) is referencing missing WDT data (FileID:{wdtFileId})", ex);	
 		}
@@ -210,7 +145,7 @@ internal class Generator
 				{
 					for (int col = 0; col < 64; col++)
 					{
-						fileReader.Skip(28);
+						fileReader.BaseStream.Position += 28;
 						var chunkId = fileReader.ReadUInt32();
 						if (chunkId > 0)
 						{
---
+++ Generator/TACTMapDBCProvider.cs
@@ -0,0 +1,26 @@
+using DBCD.Providers;
+using TACTSharp;
+
+namespace Minimaps.Generator;
+
+internal class TACTMapDBCProvider(BuildInstance buildInstance) : IDBCProvider
+{
+	public Stream StreamForTableName(string tableName, string build)
+	{
+		int fileDataID = tableName switch
+		{
+			"Map" => 1349477, // TODO: Confirm that this is stable enough to hard-code?
+			_ => throw new NotImplementedException()
+		};
+		
+		try
+		{
+			var fileBytes = buildInstance.OpenFileByFDID((uint)fileDataID);
+			return new MemoryStream(fileBytes);
+		}
+		catch (Exception ex)
+		{
+			throw new Exception($"Unable to open file with fileDataID {fileDataID} for table {tableName}", ex);
+		}
+	}
+}
