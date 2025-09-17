96353cdf41c2a030465da07afc6e668257d85ba6
John McBroom <john@mcbroom.dev>
2025-09-17 02:10:46 -0700

    Minimaps.Server baseline

---
+++ Minimaps.Server/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Minimaps.Server.Pages
+{
+	public class IndexModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+	}
+}
---
+++ Minimaps.Server/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Minimaps.Server.Pages
+{
+	public class PrivacyModel : PageModel
+	{
+		public void OnGet()
+		{
+		}
+	}
+
+}
