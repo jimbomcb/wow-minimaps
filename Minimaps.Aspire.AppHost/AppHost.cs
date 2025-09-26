bbf3bea3fa7bdd95c2c4ce8c1b9ee816628eeeea
John McBroom <john@mcbroom.dev>
2025-09-25 23:01:41 -0700

    Use external database for now

--- Minimaps.Aspire.AppHost/AppHost.cs
+++ Minimaps.Aspire.AppHost/AppHost.cs
@@ -1,12 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres")
-    .WithDataVolume("minimap-postgres-persist")
-    .WithPgAdmin(x=> {
-        x.WithHostPort(25432);
-    });
 
-var minimapsDb = postgres.AddDatabase("minimaps-database");
+//var postgres = builder.AddPostgres("postgres")
+//    //.WithDataVolume("minimap-postgres-persist")
+//    .WithPgAdmin(x=> {
+//        x.WithHostPort(25432);
+//    });
+//var minimapsDb = postgres.AddDatabase("minimaps-database");
+// Pull from ConnectionStrings__minimaps-database rather than docker contaienr for nwo
+var minimapsDb = builder.AddConnectionString("minimaps-database");
+
 
 // migrations after postgres/before app load
 var migrationService = builder.AddProject<Projects.Minimaps_CLI>("migration")
