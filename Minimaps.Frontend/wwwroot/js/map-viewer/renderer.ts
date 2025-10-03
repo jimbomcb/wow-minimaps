f1bef587419b369535067309f72e9185a446649d
John McBroom <john@mcbroom.dev>
2025-10-03 04:09:31 -0700

    Use linear scaling when shrinking/zooming out

--- Minimaps.Frontend/wwwroot/js/map-viewer/renderer.ts
+++ Minimaps.Frontend/wwwroot/js/map-viewer/renderer.ts
@@ -219,9 +219,7 @@ export class Renderer {
     }
 
     private renderTileWithTexture(x: number, y: number, texture: WebGLTexture, viewport: MapViewport): void {
-        const transform = this.createTileTransform(
-            x, y, 1.0, viewport
-        );
+        const transform = this.createTileTransform(x, y, 1.0, viewport);
         
         this.gl.activeTexture(this.gl.TEXTURE0);
         this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
@@ -294,8 +292,8 @@ export class Renderer {
         this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
         
         this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, image);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);  // linear when shrinking
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST); // nearest neighbour when magnifying
         this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
         this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
         
